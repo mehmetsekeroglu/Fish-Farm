329ad18d4c60d9eb899bd4df170b549bfc057cad
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2021-04-04 23:38:22 +0200

    css added

--- src/components/FishDetail.js
+++ src/components/FishDetail.js
@@ -1,7 +1,16 @@
+import './FishDetail.css'
+
 export default function FishDetail({fishInfo}) {
     return(
-        <div>
+        <div className="fish-info">
             <span>{fishInfo.fishType}</span>
+            <span>{fishInfo.price}</span>
+            <span>{fishInfo.durationInDays}</span>
+            <span>{fishInfo.itemWeightInGrams}</span>
+            <span>{fishInfo.originCountry}</span>
+            <span>{fishInfo.season}</span>
+            <span>{fishInfo.stockVolumeInKg}</span>
+            <span>{fishInfo.saleLocations}</span>
         </div>
     )
 }
