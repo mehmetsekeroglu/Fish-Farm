bc7d30cad0cae1a1b00e0d7ece6743c4d76b2910
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2021-04-05 00:01:39 +0200

    css update

--- src/components/FishDetail.js
+++ src/components/FishDetail.js
@@ -3,14 +3,18 @@ import './FishDetail.css'
 export default function FishDetail({fishInfo}) {
     return(
         <div className="fish-info">
-            <span>{fishInfo.fishType}</span>
+            <span id="name">{fishInfo.fishType}</span>
             <span>{fishInfo.price}</span>
             <span>{fishInfo.durationInDays}</span>
             <span>{fishInfo.itemWeightInGrams}</span>
             <span>{fishInfo.originCountry}</span>
             <span>{fishInfo.season}</span>
             <span>{fishInfo.stockVolumeInKg}</span>
-            <span>{fishInfo.saleLocations}</span>
+            <div>{fishInfo.saleLocations.map(location=>{
+                return(
+                    <div>{location}</div>
+                )
+            })}</div>
         </div>
     )
 }
